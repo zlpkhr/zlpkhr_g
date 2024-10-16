058e05e18a6a8683ff5b0f15c667377caf3efaf9
Azamat Zulpykhar <azamat_zulpykhar@icloud.com>
2024-10-16 16:21:57 +0200

    Implemented hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,25 @@ export default function Page() {
           </span>
         </div>
       </div>
+      <h2 className="mt-14 font-title font-light text-[2.5rem] leading-[1.15]">
+        Bringing <span className="text-pink">elegant</span> &{" "}
+        <span className="text-green">user-friendly</span> solutions for complex
+        challenges.
+      </h2>
+      <div className="text-neutral-800 text-base leading-[1.4] flex flex-col gap-y-[0.375rem] mt-6">
+        <p>Hey, I&#39;m Gaukhar, a product designer from Kazakhstan.</p>
+        <p>
+          I&#39;ve designed products and features for large international
+          enterprises, mid/small companies and startups in industries like Oil &
+          Gas, B2B, HRTech, EdTech. My approach blends creativity with
+          analytical thinking, focusing on empathy and collaboration to deliver
+          memorable user experiences that exceed expectations.
+        </p>
+      </div>
+      <div className="mt-10 flex gap-x-4">
+        <button className="text-neutral-50 bg-[#241F1C] py-2.5 px-4 rounded-lg font-medium text-balance leading-[1.4]">Download CV</button>
+        <button className="text-neutral-900 py-2.5 px-4 rounded-lg font-medium text-balance leading-[1.4]">Copy e-mail</button>
+      </div>
     </div>
   );
 }
