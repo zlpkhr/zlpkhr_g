db79ef54f8d7bd8d4ada6abc11cae2066aec890c
Azamat Zulpykhar <azamat_zulpykhar@icloud.com>
2024-10-16 15:39:29 +0200

    Added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,25 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          50: "#fafaf9",
+          100: "#f5f5f4",
+          200: "#e7e5e4",
+          300: "#d6d3d1",
+          400: "#a8a29e",
+          500: "#78716c",
+          600: "#57534e",
+          700: "#44403c",
+          800: "#292524",
+          900: "#1c1917",
+          950: "#0c0a09",
+        },
+        pink: "#DD84A1",
+        green: "#AEBE89",
+      },
+    },
   },
   plugins: [],
 };
