48b4706d41c47111f4a77286e6d86ddf223d9952
Azamat Zulpykhar <azamat_zulpykhar@icloud.com>
2024-10-16 15:31:24 +0200

    Updated import alias

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
+import styles from "#/styles/Home.module.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
