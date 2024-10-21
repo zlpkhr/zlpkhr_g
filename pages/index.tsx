1d5bfe037f6badaccd37f7333e4ae8c5d9b0ef5f
Azamat Zulpykhar <azamat_zulpykhar@icloud.com>
2024-10-21 18:21:00 +0200

    Refactored index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,61 @@
+import {
+  Avatar,
+  AvatarImage,
+  AvatarBio,
+  AvatarName,
+  AvatarDescription,
+} from "#/components/avatar";
 import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="mx-auto max-w-[36.25rem] pb-10 pt-[4.5rem]">
-      <div className="flex gap-x-4">
-        <Image
+    <div className="mx-auto max-w-145 pb-10 pt-18">
+      <Avatar>
+        <AvatarImage
           src="/images/avatar.webp"
-          width={48}
-          height={48}
           alt="Portrait photography of Gaukhar Zulpykhar"
-          className="rounded-full"
+          size={48}
         />
-        <div className="flex flex-col gap-y-[0.1875rem]">
-          <h1 className="text-base font-semibold leading-[1.4] text-neutral-800">
-            Gaukhar Zulpykhar
-          </h1>
-          <span className="text-base leading-[1.4] text-neutral-400">
-            Product Designer
-          </span>
+        <AvatarDescription>
+          <AvatarName>Gaukhar Zulpykhar</AvatarName>
+          <AvatarBio>Product Designer</AvatarBio>
+        </AvatarDescription>
+      </Avatar>
+      <div className="mt-14 flex flex-col gap-y-10">
+        <div className="flex flex-col gap-y-6">
+          <h2 className="font-title text-title font-light">
+            Bringing <span className="text-pink">elegant</span> &{" "}
+            <span className="text-green">user-friendly</span> solutions for
+            complex challenges.
+          </h2>
+          <div className="flex flex-col gap-y-1.5">
+            <p className="text-m text-neutral-800">
+              Hey, I&#39;m Gaukhar, a product designer from Kazakhstan.
+            </p>
+            <p className="text-m text-neutral-800">
+              I&#39;ve designed products and features for large international
+              enterprises, mid/small companies and startups in industries like
+              Oil & Gas, B2B, HRTech, EdTech. My approach blends creativity with
+              analytical thinking, focusing on empathy and collaboration to
+              deliver memorable user experiences that exceed expectations.
+            </p>
+          </div>
+        </div>
+        <div className="flex gap-x-4">
+          <button className="text-m rounded-lg bg-[#241F1C] px-4 py-2.5 font-medium text-neutral-50">
+            Download CV
+          </button>
+          <button className="text-m rounded-lg px-4 py-2.5 font-medium text-neutral-900">
+            Copy e-mail
+          </button>
         </div>
       </div>
-      <h2 className="mt-14 font-title text-[2.5rem] font-light leading-[1.15]">
-        Bringing <span className="text-pink">elegant</span> &{" "}
-        <span className="text-green">user-friendly</span> solutions for complex
-        challenges.
-      </h2>
-      <div className="mt-6 flex flex-col gap-y-[0.375rem] text-base leading-[1.4] text-neutral-800">
-        <p>Hey, I&#39;m Gaukhar, a product designer from Kazakhstan.</p>
-        <p>
-          I&#39;ve designed products and features for large international
-          enterprises, mid/small companies and startups in industries like Oil &
-          Gas, B2B, HRTech, EdTech. My approach blends creativity with
-          analytical thinking, focusing on empathy and collaboration to deliver
-          memorable user experiences that exceed expectations.
-        </p>
-      </div>
-      <div className="mt-10 flex gap-x-4">
-        <button className="rounded-lg bg-[#241F1C] px-4 py-2.5 text-base font-medium leading-[1.4] text-neutral-50">
-          Download CV
-        </button>
-        <button className="rounded-lg px-4 py-2.5 text-base font-medium leading-[1.4] text-neutral-900">
-          Copy e-mail
-        </button>
-      </div>
-      <h2 className="mt-14 text-base font-bold leading-[1.4] text-neutral-900">
+      <h2 className="text-m mt-14 font-bold text-neutral-900">
         Works
       </h2>
       <div className="mt-6 flex flex-col gap-y-6">
         <div className="relative flex flex-col gap-y-3">
-          <span className="absolute right-5 top-5 rounded-full bg-neutral-200 px-2 py-1 text-xs leading-[1.4] text-neutral-400">
+          <span className="absolute right-5 top-5 rounded-full bg-neutral-200 px-2 py-1 text-xs text-neutral-400">
             coming soon
           </span>
           <div className="aspect-[3/2] rounded-xl bg-neutral-100 px-[5.5rem] pt-[4.5rem]">
@@ -61,7 +68,7 @@ export default function Page() {
             />
           </div>
           <hgroup className="flex flex-col gap-y-[0.375rem]">
-            <h4 className="text-base leading-[1.4] text-neutral-800">
+            <h4 className="text-m leading-[1.4] text-neutral-800">
               Streamlining work processes for Chevron&#39;s joint venture
             </h4>
             <p className="text-sm leading-[1.4] text-neutral-400">
@@ -80,7 +87,7 @@ export default function Page() {
             />
           </div>
           <hgroup className="flex flex-col gap-y-[0.375rem]">
-            <h4 className="text-base leading-[1.4] text-neutral-800">
+            <h4 className="text-m leading-[1.4] text-neutral-800">
               Corporate platfrom for ExxonMobil&#39;s distributor
             </h4>
             <p className="text-sm leading-[1.4] text-neutral-400">
