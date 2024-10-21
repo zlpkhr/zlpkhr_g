37157fd1def269a4d1c4203b35591920a0d1d425
Azamat Zulpykhar <azamat_zulpykhar@icloud.com>
2024-10-21 17:26:59 +0200

    Added works section

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,8 +36,59 @@ export default function Page() {
         </p>
       </div>
       <div className="mt-10 flex gap-x-4">
-        <button className="text-neutral-50 bg-[#241F1C] py-2.5 px-4 rounded-lg font-medium text-base leading-[1.4]">Download CV</button>
-        <button className="text-neutral-900 py-2.5 px-4 rounded-lg font-medium text-base leading-[1.4]">Copy e-mail</button>
+        <button className="text-neutral-50 bg-[#241F1C] py-2.5 px-4 rounded-lg font-medium text-base leading-[1.4]">
+          Download CV
+        </button>
+        <button className="text-neutral-900 py-2.5 px-4 rounded-lg font-medium text-base leading-[1.4]">
+          Copy e-mail
+        </button>
+      </div>
+      <h2 className="font-bold text-base leading-[1.4] text-neutral-900 mt-14">
+        Works
+      </h2>
+      <div className="flex flex-col gap-y-6 mt-6">
+        <div className="flex flex-col gap-y-3 relative">
+          <span className="absolute top-5 right-5 bg-neutral-200 text-neutral-400 text-xs leading-[1.4] rounded-full px-2 py-1">
+            coming soon
+          </span>
+          <div className="bg-neutral-100 rounded-xl aspect-[3/2] pt-[4.5rem] px-[5.5rem]">
+            <Image
+              src="/images/works/chevron.jpeg"
+              alt="Streamlining work processes for Chevron&#39;s joint venture"
+              width={1920}
+              height={1117}
+              className="shadow-[0px_12px_26px_0px_#4D4D4D0A,0px_47px_47px_0px_#4D4D4D0A,0px_107px_64px_0px_#4D4D4D05,0px_189px_76px_0px_#4D4D4D03,0px_296px_83px_0px_#4D4D4D00] rounded"
+            />
+          </div>
+          <hgroup className="flex flex-col gap-y-[0.375rem]">
+            <h4 className="text-base leading-[1.4] text-neutral-800">
+              Streamlining work processes for Chevron&#39;s joint venture
+            </h4>
+            <p className="text-sm leading-[1.4] text-neutral-400">
+              2023 - 2024
+            </p>
+          </hgroup>
+        </div>
+        <div className="flex flex-col gap-y-3 relative">
+          <div className="bg-neutral-100 rounded-xl aspect-[3/2] pt-[4.5rem] px-[5.5rem]">
+            <Image
+              src="/images/works/exxon-mobil.png"
+              alt="Corporate platfrom for ExxonMobil&#39;s distributor"
+              width={1440}
+              height={1024}
+              className="shadow-[0px_12px_26px_0px_#4D4D4D0A,0px_47px_47px_0px_#4D4D4D0A,0px_107px_64px_0px_#4D4D4D05,0px_189px_76px_0px_#4D4D4D03,0px_296px_83px_0px_#4D4D4D00] rounded"
+            />
+          </div>
+          <hgroup className="flex flex-col gap-y-[0.375rem]">
+            <h4 className="text-base leading-[1.4] text-neutral-800">
+              Corporate platfrom for ExxonMobil&#39;s distributor
+            </h4>
+            <p className="text-sm leading-[1.4] text-neutral-400">
+              2022 - 2023
+            </p>
+          </hgroup>
+        </div>
+        <div></div>
       </div>
     </div>
   );
